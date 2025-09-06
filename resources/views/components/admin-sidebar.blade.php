db225279af04254048c3c2cea298fbe98b50aafd
chesyaaa <fatimahauliarachaman@gmail.com>
2025-09-06 14:18:05 +0700

    Update sidebar UI dengan logo dan CSS

--- resources/views/components/admin-sidebar.blade.php
+++ resources/views/components/admin-sidebar.blade.php
@@ -1,9 +1,30 @@
 <div class="sidebar">
-    <ul>
-
-        <li><a href="{{ route('dashboard') }}">Dashboard</a></li>
-        <li><a href="{{ route('daftarKasir') }}">Daftar Kasir</a></li>
-        <li><a href="{{ route('manajemenMenu') }}">Manajemen Menu</a></li>
-        <li><a href="{{ route('logout') }}">Logout</a></li>
+    <div class="sidebar-header">
+        <img src="{{ asset('images/logok4cafe.png') }}" alt="Logo K4 Cafe" class="sidebar-logo">
+    </div>
+    <ul class="sidebar-menu">
+        <li>
+            <a href="{{ route('dashboard') }}">
+                <span class="icon">👓</span> Dashboard
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('daftarKasir') }}">
+                <span class="icon">👥</span> Daftar Kasir
+            </a>
+        </li>
+        <li>
+            <a href="{{ route('manajemenMenu') }}">
+                <span class="icon">☰</span> Manajemen Menu
+            </a>
+        </li>
     </ul>
+    <div class="sidebar-footer">
+        <a href="{{ route('logout') }}">
+            <span class="icon">↪️</span> Logout
+        </a>
+    </div>
 </div>
+
+{{-- panggil file css --}}
+<link rel="stylesheet" href="{{ asset('css/sidebar.css') }}">
